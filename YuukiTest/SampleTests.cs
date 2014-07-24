eebd9a4b5bd0c821f389fa19e703bbd65c274b9e
libsora.so <libsora25@gmail.com>
2014-07-24 21:22:12 +0900

    Yuuki에 대응하는 유닛테스트

---
+++ YuukiTest/SampleTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Yuuki;
+
+namespace YuukiTest
+{
+    public class SampleTests
+    {
+        [TestCase]
+        public void TestFoo()
+        {
+            int a = Helper.Foo();
+            Assert.AreEqual(a, 1);
+        }
+    }
+}
