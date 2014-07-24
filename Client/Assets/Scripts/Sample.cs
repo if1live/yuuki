6e4b8e91059394b6863c8beeeb2e84fc6444275b
libsora.so <libsora25@gmail.com>
2014-07-24 21:12:30 +0900

    유니티+공용라이브러리+c#서버 ㅜ조

---
+++ Client/Assets/Scripts/Sample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int a = Yuuki.Helper.Foo();
+        Debug.Log("Hello World = " + a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Yuuki/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yuuki
+{
+    public class Helper
+    {
+        public static int Foo()
+        {
+            return 1;
+        }
+    }
+}
