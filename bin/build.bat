8c982ab8453a835fa25323525e7b2bb2c42c2a2e
lacti <lactrious@gmail.com>
2014-07-28 22:41:23 +0900

    append build command of 'Yuuki' solution to build script (build.bat)

--- bin/build.bat
+++ bin/build.bat
@@ -8,6 +8,7 @@
 
 @set UNITY_PATH="c:\Program Files (x86)\Unity\Editor\Unity.exe"
 
+"%VS120COMNTOOLS%\..\IDE\devenv.exe" /build Release %BASE_DIR%Yuuki.sln
 %UNITY_PATH% -quit -batchmode -projectPath %WIN_PROJ_DIR% -executeMethod ScriptBatch.BuildWinGame
 %UNITY_PATH% -quit -batchmode -projectPath %ANDROID_PROJ_DIR% -executeMethod ScriptBatch.BuildAndroidGame
 %UNITY_PATH% -quit -batchmode -projectPath %WEB_PROJ_DIR% -executeMethod ScriptBatch.BuildWebGame
